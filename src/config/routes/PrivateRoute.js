b96f0bedcce8fa4a8b5724cb402d6b6dddc52735
Muhamad Allya Agung <mallyaagung@gmail.com>
2025-12-05 21:58:50 +0700

    chore: routing kalo isAuth false

--- src/config/routes/PrivateRoute.js
+++ src/config/routes/PrivateRoute.js
@@ -3,9 +3,14 @@ import { Navigate, useLocation } from 'react-router-dom'
 
 const PrivateRoute = ({ element: Component, allowedUsers }) => {
   const role = useSelector((state) => state.user.role)
+  const isAuth = useSelector((state) => state.user.isAuth)
 
-  if (!allowedUsers.includes(role)) {
-    return <Navigate to={'/report'} replace />
+  if (isAuth) {
+    if (!allowedUsers.includes(role)) {
+      return <Navigate to={'/report'} replace />
+    }
+  } else {
+    return <Navigate to={'/login'} replace />
   }
 
   return <Component />
